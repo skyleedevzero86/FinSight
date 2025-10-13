5fbfac4295e2926bd0cf71f2dbff33b5851dba06
이경용 <sleekydz86@icloud.com>
2025-10-13 21:01:34 +0900

    feat:  정리

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((c) => c + 1)}>
-          count is {count}
+          숫자 is' 뭔들 {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
