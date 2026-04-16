1118efad6e19f7a51bd4415160012563aeede7ba
이경용 <sleekydz86@naver.com>
2026-04-16 19:15:49 +0900

    fix: JobRepository 빈 누락으로 인한 배치 기동 오류 수정
    - 본문 BatchApplication에 @EnableBatchProcessing을 추가해 Spring Batch 인프라 빈이 생성되도록 변경
    -   BoardModerationJobConfig 주입 실패로 앱 기동이 중단되던 문제를 해결

--- backend/batch/src/main/java/com/sleekydz86/BatchApplication.java
+++ backend/batch/src/main/java/com/sleekydz86/BatchApplication.java
@@ -2,11 +2,13 @@ package com.sleekydz86;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableScheduling
 @EnableTransactionManagement
+@EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
 
